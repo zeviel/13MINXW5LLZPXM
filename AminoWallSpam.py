b22e7e2acf96f837d34dd1500ec7b04905437b57
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-04-01 22:56:20 +0800

    Update AminoWallSpam.py

--- AminoWallSpam.py
+++ AminoWallSpam.py
@@ -12,22 +12,27 @@ print("YouTube: https://www.youtube.com/channel/UCJ61JlXJckmO6yJr8BDRuGQ")
 print('▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ █░░░█ ▄▀▄ █░░ █░░ ▄▀▀ █▀▄ ▄▀▄ █▄░▄█')
 print("█▀█ █░█░█ █ █░▀█ █░█ █░█░█ █▀█ █░▄ █░▄ ░▀▄ █░█ █▀█ █░█░█")
 print("▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ░▀░▀░ ▀░▀ ▀▀▀ ▀▀▀ ▀▀░ █▀░ ▀░▀ ▀░░░▀")
+communities = {}
 import amino
-#инпут почты и пароля чтобы бот зашел в аккаунт
-email=input("Email/Почта:")
-password=input("Password/Пароль:")
+email = input("Email/Почта:")
+password = input("Password/Пароль:")
 client = amino.Client()
 client.login(email=email, password=password)
-for name, id in zip(client.sub_clients().name, client.sub_clients().comId):
-    print(f"{name}: {id}")
-comid = input("Выберите сообщество(id): ")
-sub_client = amino.SubClient(comId=comid,profile=client.profile)
+clients = client.sub_clients(size=100)
+x = 0
+for name, id in zip(clients.name, clients.comId):
+    print(f"{x + 1}.{name}")
+    communities[x] = str(id)
+    x+=1
+
+communityid = communities[int(input("Выберите сообщество/Select the community: "))-1]
+sub_client = amino.SubClient(comId=communityid, profile=client.profile)
+
 print('\nLogged in/Бот зашел!')
 userlink=input("Type user link/Введите ссылку на пользователя: ")
 user=client.get_from_code(userlink)
 userId=user.objectId
-comId=user.path[1:user.path.index('/')]
-subclient=amino.SubClient(comId=comid ,profile=client.profile)
+sub_client=amino.SubClient(comId=communityid ,profile=client.profile)
 comment=input("Message/Сообщение:")
 while True:
 	try:
