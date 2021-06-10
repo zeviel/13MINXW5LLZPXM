5ada2b413ae26d0b92298c7a25aabc22e858a229
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-06-10 14:08:27 +0600

    Update AminoWallSpam.py

--- AminoWallSpam.py
+++ AminoWallSpam.py
@@ -1,17 +1,15 @@
-from colorama import init
-from colorama import Fore, Back, Style
+import amino
+import pyfiglet
+from colorama import init, Fore, Back, Style
 init()
 print(Back.BLACK)
 print(Fore.CYAN)
 print("""Script by Lil Zevi
-Github : https://github.com/LilZevi
-▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ █░░░█ ▄▀▄ █░░ █░░ ▄▀▀ █▀▄ ▄▀▄ █▄░▄█
-█▀█ █░█░█ █ █░▀█ █░█ █░█░█ █▀█ █░▄ █░▄ ░▀▄ █░█ █▀█ █░█░█
-▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ░▀░▀░ ▀░▀ ▀▀▀ ▀▀▀ ▀▀░ █▀░ ▀░▀ ▀░░░▀""")
-import amino
-email = input("Email/Почта:")
-password = input("Password/Пароль:")
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("aminowallspam", font="stop"))
 client = amino.Client()
+email = input("Email/Почта: ")
+password = input("Password/Пароль: ")
 client.login(email=email, password=password)
 clients = client.sub_clients(start=0, size=1000)
 for x, name in enumerate(clients.name, 1):
@@ -19,15 +17,14 @@ for x, name in enumerate(clients.name, 1):
 communityid = clients.comId[int(input("Выберите сообщество/Select the community: "))-1]
 sub_client = amino.SubClient(comId=communityid, profile=client.profile)
 
-print('\nLogged in/Бот зашел!')
-userlink=input("Type user link/Введите ссылку на пользователя: ")
-user=client.get_from_code(userlink)
-userId=user.objectId
-sub_client=amino.SubClient(comId=communityid ,profile=client.profile)
-comment=input("Message/Сообщение:")
+userlink = input("Type user link/Введите ссылку на пользователя: ")
+user = client.get_from_code(userlink)
+userId = user.objectId
+sub_client = amino.SubClient(comId=communityid ,profile=client.profile)
+comment = input("Message/Сообщение:")
 while True:
 	try:
-		sub_client.comment(message=comment,userId=userId)
+		sub_client.comment(message=comment, userId=userId)
 		print("WallComment Sended/Комментарий на стену отправлен!")
 	except:
 		pass
