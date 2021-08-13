6497451b4a4406d55a4f9fbf4a2c494b4d114422
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-08-14 01:19:15 +0600

    Update AminoWallSpam.py

--- AminoWallSpam.py
+++ AminoWallSpam.py
@@ -1,30 +1,32 @@
 import amino
+import asyncio
 import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
-print(Back.BLACK)
 print(Fore.CYAN)
 print("""Script by Lil Zevi
 Github : https://github.com/LilZevi""")
 print(pyfiglet.figlet_format("aminowallspam", font="stop"))
-client = amino.Client()
-email = input("Email/Почта: ")
-password = input("Password/Пароль: ")
-client.login(email=email, password=password)
-clients = client.sub_clients(start=0, size=1000)
-for x, name in enumerate(clients.name, 1):
-    print(f"{x}.{name}")
-communityid = clients.comId[int(input("Выберите сообщество/Select the community: "))-1]
-sub_client = amino.SubClient(comId=communityid, profile=client.profile)
 
-userlink = input("Type user link/Введите ссылку на пользователя: ")
-user = client.get_from_code(userlink)
-userId = user.objectId
-sub_client = amino.SubClient(comId=communityid ,profile=client.profile)
-comment = input("Message/Сообщение:")
-while True:
-	try:
-		sub_client.comment(message=comment, userId=userId)
-		print("WallComment Sended/Комментарий на стену отправлен!")
-	except:
-		pass
+async def main():
+	client = amino.Client()
+	email = input("Email >> ")
+	password = input("Password >> ")
+	await client.login(email=email, password=password)
+	clients = await client.sub_clients(start=0, size=100)
+	for x, name in enumerate(clients.name, 1):
+		print(f"{x}.{name}")
+	communityid = clients.comId[int(input("Select the community >> "))-1]
+	sub_client = await amino.SubClient(comId=communityid, profile=client.profile)
+	userlink = input("User Link >> ")
+	user_info = await client.get_from_code(userlink)
+	userId = user_info.objectId
+	comment = input("Message >> ")
+	while True:
+		try:
+			await sub_client.comment(message=comment, userId=userId)
+			print("WallComment Sended")
+		except:
+			pass
+
+asyncio.get_event_loop().run_until_complete(main())
