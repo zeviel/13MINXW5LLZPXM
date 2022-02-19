c6fc37c51de46c5082fa16ea555e6f6f9a39476f
Zghzvzzbbsv <99689835+Zghzvzzbbsv@users.noreply.github.com>
2022-02-19 16:10:01 +0300

    Update AminoWallSpam.py

--- AminoWallSpam.py
+++ AminoWallSpam.py
@@ -1,25 +1,24 @@
-import amino
+import aminofix
 from pyfiglet import figlet_format
 from colorama import init, Fore, Style
 init()
 print(
     f"""{Fore.CYAN}
-Script by deluvsushi
-Github : https://github.com/deluvsushi"""
+Script by lilkek
+TG : https://t.me/two_brothers_2"""
 )
 print(figlet_format("aminowallspam", font="stop"))
-client = amino.Client()
+client = aminofix.Client()
 email = input("-- Email::: ")
 password = input("-- Password::: ")
 client.login(email=email, password=password)
 link_info = client.get_from_code(input("-- User link::: ")).json
 com_id = link_info["extensions"]["linkInfo"]["ndcId"]
 user_id = link_info["extensions"]["linkInfo"]["objectId"]
-sub_client = amino.SubClient(comId=com_id, profile=client.profile)
 message = input("-- Message::: ")
 while True:
 	try:
-		sub_client.comment(userId=user_id, message=message)
+		client.comment(userId=user_id, message=message)
 		print("-- Comment is sent...")
 	except Exception as e:
 		print(e)
