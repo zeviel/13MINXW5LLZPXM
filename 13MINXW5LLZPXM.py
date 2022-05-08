5ce9013618a88101df578406941267ee316b4e10
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-05-08 22:13:35 +0800

    Update and rename AminoWallSpam.py to 13MINXW5LLZPXM.py

--- 13MINXW5LLZPXM.py
+++ 13MINXW5LLZPXM.py
@@ -7,14 +7,14 @@ print(
 Script by deluvsushi
 Github : https://github.com/deluvsushi"""
 )
-print(figlet_format("aminowallspam", font="stop"))
+print(figlet_format("13MINXW5LLZPXM", font="stop"))
 client = amino.Client()
 email = input("-- Email::: ")
 password = input("-- Password::: ")
 client.login(email=email, password=password)
-link_info = client.get_from_code(input("-- User link::: ")).json
-com_id = link_info["extensions"]["linkInfo"]["ndcId"]
-user_id = link_info["extensions"]["linkInfo"]["objectId"]
+link_info = client.get_from_code(input("-- User link::: ")).json["extensions"]["linkInfo"]
+com_id = link_info["ndcId"]
+user_id = link_info["objectId"]
 sub_client = amino.SubClient(comId=com_id, profile=client.profile)
 message = input("-- Message::: ")
 while True:
