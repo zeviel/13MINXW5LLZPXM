0c435896871ef175b272e3402d832c4b091d3fa4
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-25 15:21:41 +0800

    Update 13MINXW5LLZPXM.py

--- 13MINXW5LLZPXM.py
+++ 13MINXW5LLZPXM.py
@@ -4,8 +4,8 @@ from colorama import init, Fore, Style
 init()
 print(
     f"""{Fore.CYAN}
-Script by deluvsushi
-Github : https://github.com/deluvsushi"""
+Script by zeviel
+Github : https://github.com/zeviel"""
 )
 print(figlet_format("13MINXW5LLZPXM", font="stop"))
 client = amino.Client()
